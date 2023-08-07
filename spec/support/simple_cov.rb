b0536d95dc600e18007d277f34ca74e6195faa02
Rivelino Junior <rv.fernandes.junior@gmail.com>
2023-08-07 18:08:13 -0300

    Add simplecov json extension

--- spec/support/simple_cov.rb
+++ spec/support/simple_cov.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 require 'simplecov'
+require 'simplecov-json'
 
-SimpleCov.start 'rails'
+SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new(
+  [SimpleCov::Formatter::HTMLFormatter, SimpleCov::Formatter::JSONFormatter]
+)
+
+SimpleCov.start 'rails' do
+  enable_coverage :branch
+  add_group 'Services', 'app/services'
+end
