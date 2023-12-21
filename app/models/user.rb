68be2cee0a16de773bcc043a32a28fc648698b1a
Rivelino Junior <rv.fernandes.junior@gmail.com>
2023-12-21 00:46:09 -0300

    use the bcrypt gem to store the user's password safely.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_secure_password
+
   validates :full_name, :username, presence: true
   validates :email, presence: true, email: true, uniqueness: true
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,5 +5,6 @@ FactoryBot.define do
     email { Faker::Internet.unique.email }
     full_name { Faker::Name.name }
     username { Faker::Internet.username }
+    password { Faker::Internet.password }
   end
 end
