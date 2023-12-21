a7fc80da51b7b628449f5053c6d3471d318a845a
Rivelino Junior <rv.fernandes.junior@gmail.com>
2023-12-21 00:41:11 -0300

    run db:migrate

--- db/migrate/20231221033610_create_users.rb
+++ db/migrate/20231221033610_create_users.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
@@ -7,6 +9,8 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.string :password_digest
 
       t.timestamps
+
+      t.index :email, unique: true
     end
   end
 end
