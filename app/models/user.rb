16fa7dd86e2b84bb46c9d4542372e0e5ce6857c9
Rivelino Junior <rv.fernandes.junior@gmail.com>
2023-12-21 00:39:16 -0300

    add validations for the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  validates :full_name, :username, presence: true
+  validates :email, presence: true, email: true, uniqueness: true
 end
