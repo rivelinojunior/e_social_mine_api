afba72c71e785a71c41d31a411f592186ca1a9a7
Rivelino Junior <rv.fernandes.junior@gmail.com>
2024-06-13 23:58:01 -0300

    create query object to retrieve the current session

---
+++ app/queries/auth/current_session_query.rb
@@ -0,0 +1,19 @@
+module Auth
+  class CurrentSessionQuery < ApplicationQuery
+    def initialize(token:)
+      self.token = token
+    end
+
+    def query
+      session = Session.find_by!(token:)
+
+      Result::Success(:ok, session:, user: session.user)
+    rescue ActiveRecord::RecordNotFound => e
+      Result::Failure(:not_found, errosr: [e.message])
+    end
+
+    private
+
+    attr_accessor :token
+  end
+end
